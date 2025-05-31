dbb68d7bd3429738268e161b5173bb6836b11e32
bindu <davuluribindu995@gmail.com>
2025-05-31 22:58:21 +0530

    modified file

--- roboshop.sh
+++ roboshop.sh
@@ -6,16 +6,16 @@ Instance=("mongodb" "cart" "catalogue" "mysql" "frontend" "payment" "shipping" "
 Zone_Id=Z04547231YPUT2HMMPAFC
 Domain_Name="devops84.site"
 
-for instance in ${Instance[@]}
+for instance in ${ Instance[@] }
 do
-Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0897c394fc2d256af --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+  Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0897c394fc2d256af --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
-if [ $instance != "frontend"]
-then 
-    Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-else
-    Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].publicIpAddress" --output text)
-fi
-echo $instance Ip adress : $IP
+   if [ $instance != "frontend" ]
+   then 
+       Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+   else
+       Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].publicIpAddress" --output text)
+   fi
+  echo $instance Ip adress : $IP
 
 done
