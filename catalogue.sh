17a16373850f6c2394e33b84bf736c4678d84843
bindu <davuluribindu995@gmail.com>
2025-06-09 11:03:47 +0530

    modified file

--- catalogue.sh
+++ catalogue.sh
@@ -73,11 +73,13 @@ cp $script_dir/mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "coping mongo repo file"
 
 dnf install mongodb-mongosh -y &>>$log_file
+VALIDATE $? "installing mongodb"
 
-Status=$(mongosh --host mongodb.devops84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+Status=$(mongosh --hostmongo mongodb.devops84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $Status -lt 0 ]
 then
    mongosh --host mongodb.devops84s.site </app/db/master-data.js &>>$log_file
+   VALIDATE $? "loading  data in mongodb"
 else 
    echo -e "already data loaded"
 fi
