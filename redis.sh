b976a49128c47dc089c1bc9e198cf059c8a8a6f3
bindu <davuluribindu995@gmail.com>
2025-06-09 17:57:02 +0530

    modified file

--- redis.sh
+++ redis.sh
@@ -36,7 +36,7 @@ dnf module enable redis:7 -y
 dnf install redis -y 
 VALIDATE $? "installing the redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $?  " replacing the port "
 
 systemctl enable redis
