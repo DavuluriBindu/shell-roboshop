8fe291a4a366e4b9b4cfc6956b360a9871b26f44
bindu <davuluribindu995@gmail.com>
2025-06-10 19:52:20 +0530

    modified file

--- shipping.sh
+++ shipping.sh
@@ -73,10 +73,16 @@ VALIDATE $? "starting the shipping process"
 dnf install mysql -y 
 VALIDATE $? "instal mysql into shipping"
 
-mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
-mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
-mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw  < /app/db/master-data.sql
-VALIDATE $? "loading data to mysql"
+mysql -h mysql.devops84.site -u root -p$Mysql_root_pasw -e 'use cities'
+if [ $? -ne 0 ]
+then 
+  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
+  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
+  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw  < /app/db/master-data.sql
+ VALIDATE $? "loading data to mysql"
+else
+   echo -e "data already present "
+fi
 
 systemctl restart shipping
 VALIDATE $? "restarting shipping"
